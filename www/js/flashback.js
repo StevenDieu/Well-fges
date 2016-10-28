7995c7c132203421609bd5ac7c7fc111c2020f01
Steven Dieu <groover.Dieu@gmail.com>
2016-10-28 20:36:43 +0200

    fix bug

--- www/js/flashback.js
+++ www/js/flashback.js
@@ -1,46 +1,65 @@
 var page = 1;
 var flagAjax = true;
+var isLast = false;
 
 function getListeFlashabck(showListe, showWait) {
-    flagAjax = false;
-    if (showWait) {
-        $("#loaderListe").removeClass("none");
-        $("#emplacementWaiterlist").addClass("none");
-    }
-    $.getJSON(url + "api/listeFlashback/" + page, function (datas) {
-        jQuery.each(datas, function (i, val) {
-            $("#listeArticle").append('\
+    if (!isLast) {
+        flagAjax = false;
+        if (showWait) {
+            $("#loaderListe").removeClass("none");
+            $("#emplacementWaiterlist").addClass("none");
+        }
+        $.getJSON(url + "api/listeFlashback/" + page, function (datas) {
+            if (!isLast) {
+                if (datas.error === undefined) {
+                    jQuery.each(datas, function (i, val) {
+                        $("#listeArticle").append('\
                         <a href="./article-flashback.html?id=' + val.id + '">\n\
                         <div class="article">\n\
                         <span>' + val.titre + '</span> - \n\
                         <span class="date_debut">' + val.date_debut + '</span>\n\
                         </div>\n\
                         </a>');
+                    });
+                    if (showListe) {
+                        $("#loader").addClass("none");
+                        $("#listeArticle").removeClass("none");
+                    }
+                    if (showWait) {
+                        $("#loaderListe").addClass("none");
+                        $("#emplacementWaiterlist").removeClass("none");
+                    }
+                    if (addPage) {
+                        page++;
+                    }
+                    flagAjax = true;
+                } else {
+                    isLast = true;
+                    if ($(".article").length === 0) {
+                        $("#loader").addClass("none");
+                        $("#listeArticle").removeClass("none");
+                        $("#listeArticle").html('<div class="article"><span>Il n\'y pas de flashback. Revenez plus tard.</span></div>')
+                    } else {
+                        $("#loaderListe").addClass("none");
+                        $("#emplacementWaiterlist").removeClass("none");
+                    }
+                }
+            }
         });
-        if (showListe) {
-            $("#loader").addClass("none");
-            $("#listeArticle").removeClass("none");
-        }
-        if (showWait) {
-            $("#loaderListe").addClass("none");
-            $("#emplacementWaiterlist").removeClass("none");
-        }
-        page++;
-        flagAjax = true;
-
-    });
+    }
 }
 
 
 $(window).scroll(function () {
     if ($(window).scrollTop() + $(window).height() == $(document).height()) {
         if (flagAjax) {
-            getListeFlashabck(false, true);
+            getListeFlashabck(false, true, true);
         }
     }
 });
 
 $(function () {
-    getListeFlashabck(false, false);
-    getListeFlashabck(true, false);
+    getListeFlashabck(false, false, true);
+    page++;
+    getListeFlashabck(true, false, false);
 });
--- www/js/lesphotos.js
+++ www/js/lesphotos.js
@@ -1,46 +1,65 @@
 var page = 1;
 var flagAjax = true;
+var isLast = false;
 
-function getListeAlbum(showListe, showWait) {
-    flagAjax = false;
-    if (showWait) {
-        $("#loaderListe").removeClass("none");
-        $("#emplacementWaiterlist").addClass("none");
-    }
-    $.getJSON(url + "api/listeAlbum/" + page, function (datas) {
-        jQuery.each(datas, function (i, val) {
-            $("#listeArticle").append('\
+function getListeAlbum(showListe, showWait, addPage) {
+    if (!isLast) {
+        flagAjax = false;
+        if (showWait) {
+            $("#loaderListe").removeClass("none");
+            $("#emplacementWaiterlist").addClass("none");
+        }
+        $.getJSON(url + "api/listeAlbum/" + page, function (datas) {
+            if (!isLast) {
+                if (datas.error === undefined) {
+                    jQuery.each(datas, function (i, val) {
+                        $("#listeArticle").append('\
                         <a href="./article-lesphotos.html?id=' + val.id + '">\n\
                         <div class="article">\n\
                         <span>' + val.titre + '</span> - \n\
                         <span class="date_debut">' + val.date_debut + '</span>\n\
                         </div>\n\
                         </a>');
+                    });
+                    if (showListe) {
+                        $("#loader").addClass("none");
+                        $("#listeArticle").removeClass("none");
+                    }
+                    if (showWait) {
+                        $("#loaderListe").addClass("none");
+                        $("#emplacementWaiterlist").removeClass("none");
+                    }
+                    if (addPage) {
+                        page++;
+                    }
+                    flagAjax = true;
+                } else {
+                    isLast = true;
+                    if (page === 1) {
+                        $("#loader").addClass("none");
+                        $("#listeArticle").removeClass("none");
+                        $("#listeArticle").html('<div class="article"><span>Il n\'y pas d\'album. Revenez plus tard.</span></div>')
+                    } else {
+                        $("#loaderListe").addClass("none");
+                        $("#emplacementWaiterlist").removeClass("none");
+                    }
+                }
+            }
         });
-        if (showListe) {
-            $("#loader").addClass("none");
-            $("#listeArticle").removeClass("none");
-        }
-        if (showWait) {
-            $("#loaderListe").addClass("none");
-            $("#emplacementWaiterlist").removeClass("none");
-        }
-        page++;
-        flagAjax = true;
-
-    });
+    }
 }
 
 
 $(window).scroll(function () {
     if ($(window).scrollTop() + $(window).height() == $(document).height()) {
         if (flagAjax) {
-            getListeAlbum(false, true);
+            getListeAlbum(false, true, true);
         }
     }
 });
 
 $(function () {
-    getListeAlbum(false, false);
-    getListeAlbum(true, false);
+    getListeAlbum(false, false, true);
+    page++;
+    getListeAlbum(true, false, false);
 });
